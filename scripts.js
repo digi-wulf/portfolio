65d1877bfce0bb1f61e96832a6444fa21d491716
Adreanna <81601335+adrea-m316@users.noreply.github.com>
2022-05-16 20:27:31 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,22 @@
+function openSlideMenu() {
+    document.getElementById('side-menu').style.width = '250px';
+    document.getElementById('main').style.marginRight = '250px';
+}
+function closeSideMenu() {
+    document.getElementById('side-menu').style.width = '0px';
+    document.getElementById('main').style.marginRight = '0px';
+}
+
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("nav-bar").style.top = "0";
+    document.getElementById("nav-bar").style.opacity = "100";
+    document.getElementById("nav-bar").style.transition = ".5s";
+  } else {
+    document.getElementById("nav-bar").style.top = "-63px";
+    document.getElementById("nav-bar").style.opacity = "0";
+  }
+}
