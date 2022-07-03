f4c06eb201903edd51c5c93ae0c297d5873c8ee8
Adreanna <adreannam316@gmail.com>
2022-07-03 16:20:49 -0500

    Portfolio page updated
    Added Black Bear Cafe and updated Resume

---
+++ projects/BlackBearCafe/scripts.js
@@ -0,0 +1,9 @@
+function openSlideMenu() {
+    document.getElementById('side-menu').style.width = '250px';
+    document.getElementsById('side-menu').style.boxShadow = '-36px 0px 24px 10px rgba(46, 39, 34, .84)';
+}
+
+function closeSideMenu() {
+    document.getElementById('side-menu').style.width = '0px';
+    document.getElementsById('side-menu').style.boxShadow = '-149px 0px 24px 10px rgba(46, 39, 34, 0)';
+}
